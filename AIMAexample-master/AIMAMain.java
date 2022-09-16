0ecdff8ae44089acc6ac396fa267f6ba1b352128
Kerbolerr <david.galvez.alcantara@estudiantat.upc.edu>
2022-09-16 14:13:13 +0200

    Example working

--- AIMAexample-master/AIMAMain.java
+++ AIMAexample-master/AIMAMain.java
@@ -19,49 +19,49 @@ public class AIMAMain {
          *  For a problem to be solvable:
          *    count(0,prob) % 2 == count(0,sol) %2
          */
-        int [] prob = new int []{1 ,0, 1, 1, 0};
-        int [] sol = new int[]{1, 1, 0, 1, 0};
+        int [] prob = new int []{1 ,1,1,1,1,1, 1, 1, 0,0,0,1,1,0,0,1,1};
+        int [] sol = new int[]{0 ,1,0,1,0,1, 1, 0, 0,1,1,0,0,1,1,0,0};
 
         ProbIA5Board board = new ProbIA5Board(prob, sol );
 
         // Create the Problem object
         Problem p = new  Problem(board,
-                                new ProbIA5SuccesorFunction(),
-                                new ProbIA5GoalTest(),
-                                new ProbIA5HeuristicFunction());
+                new ProbIA5SuccesorFunction(),
+                new ProbIA5GoalTest(),
+                new ProbIA5HeuristicFunction());
 
         // Instantiate the search algorithm
-	// AStarSearch(new GraphSearch()) or IterativeDeepeningAStarSearch()
+        // AStarSearch(new GraphSearch()) or IterativeDeepeningAStarSearch()
         Search alg = new AStarSearch(new GraphSearch());
 
         // Instantiate the SearchAgent object
         SearchAgent agent = new SearchAgent(p, alg);
 
-	// We print the results of the search
-        System.out.println();
+        // We print the results of the search
         printActions(agent.getActions());
         printInstrumentation(agent.getInstrumentation());
 
         // You can access also to the goal state using the
-	// method getGoalState of class Search
+        // method getGoalState of class Search
 
     }
 
-        private static void printInstrumentation(Properties properties) {
+    private static void printInstrumentation(Properties properties) {
         Iterator keys = properties.keySet().iterator();
         while (keys.hasNext()) {
             String key = (String) keys.next();
             String property = properties.getProperty(key);
             System.out.println(key + " : " + property);
         }
-        
+
     }
-    
+
     private static void printActions(List actions) {
         for (int i = 0; i < actions.size(); i++) {
             String action = (String) actions.get(i);
             System.out.println(action);
         }
     }
-    
+
 }
+
--- AIMAexample-master/IA/ProbIA5/ProbIA5SuccesorFunction.java
+++ AIMAexample-master/IA/ProbIA5/ProbIA5SuccesorFunction.java
@@ -12,12 +12,21 @@ public class ProbIA5SuccesorFunction implements SuccessorFunction{
 
     public List getSuccessors(Object state){
         ArrayList retval = new ArrayList();
-        ProbIA5Board board = (ProbIA5Board) state;
+        ProbIA5Board status = (ProbIA5Board) state;
+
 
         // Some code here
         // (flip all the consecutive pairs of coins and generate new states
         // Add the states to retval as Succesor("flip i j", new_state)
         // new_state has to be a copy of state
+
+        for(int i=0;i<status.obtainBoard().length-1;++i){
+            ProbIA5Board probIA5Board = new ProbIA5Board(status.obtainBoard(),status.obtainGoal());
+            probIA5Board.flip_it(i);
+            Successor successor = new Successor("flip "+Integer.toString(i)+" "+Integer.toString(i+1),probIA5Board);
+            retval.add(successor);
+        }
+
 
         return retval;
 
